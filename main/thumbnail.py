9f5ff1682e8e585edf961fa68cd5550312450063
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-06 19:41:03 +0530

    Update thumbnail.py

--- main/thumbnail.py
+++ main/thumbnail.py
@@ -1,1 +1,15 @@
+from pyrogram import Client, filters 
+from config import THUMBNAIL, ADMIN
+
+@Client.on_message(filters.private & filters.command("set") & filters.user(ADMIN))                            
+async def (bot, msg):
+    print("soon..")
+
+
+
+
+
+
+
+
 
