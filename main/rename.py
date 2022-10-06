030bd1e08ce5fcdd975b3457788b2e4b5dc7cdec
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-06 23:21:02 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -6,8 +6,7 @@ from main.utils import progress_message
 @Client.on_message(filters.command("rename"))             
 async def rename_file(bot, msg):
     reply = msg.reply_to_message
-    file_name = msg.text.split(" ", 1)[1]
-    new_name = f"./DOWNLOADS/{file_name}"
+    new_name = msg.text.split(" ", 1)[1]
     sts = await msg.reply_text("Trying to Downloading.....")
     c_time = time.time()
     downloaded = await reply.download(
@@ -21,17 +20,32 @@ async def rename_file(bot, msg):
         cap = f"{new_name}"
     raw_thumbnail = temp.THUMBNAIL 
     if raw_thumbnail:
-       og_thumbnail = await bot.download_media(raw_thumbnail)
-    await sts.edit("Trying to Uploading")
-    c_time = time.time()
-    await bot.send_document(
-            msg.chat.id,
-            document=downloaded,
-            thumb=og_thumbnail, 
-            caption=cap, 
-            progress=progress_message,
-            progress_args=("Uploade Started.....", sts, c_time)
-    )      
+        og_thumbnail = await bot.download_media(raw_thumbnail)
+        await sts.edit("Trying to Uploading")
+        c_time = time.time()
+        await bot.send_document(msg.chat.id, document=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))                                    
+        try:
+           os.remove(downloaded)
+           os.remove(og_thumbnail)
+        except:
+           pass
+        await sts.delete()
+    else:
+        await sts.edit("Trying to Uploading")
+        c_time = time.time()
+        await bot.send_document(msg.chat.id, document=downloaded, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))                   
+        try:
+           os.remove(downloaded)          
+        except:
+           pass
+        await sts.delete()        
+
+
+
+
+
+
+
 
 
 
