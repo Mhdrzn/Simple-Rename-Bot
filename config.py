d0b3b51a1c3597751338196b6da588efd74fcf91
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-06 19:23:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+import os.environ
+
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+ADMIN = int(environ.get("ADMIN", ""))
+THUMBNAIL = environ.get("THUMBNAIL", "")
+CAPTION = environ.get("CAPTION", "")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
