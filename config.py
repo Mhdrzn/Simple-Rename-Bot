eeab51b7d3fc913f5570259a21b65fab56fb01b6
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-06 20:47:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))
-THUMBNAIL = [set(x) for x in environ.get("THUMBNAIL", "")]                       
 CAPTION = environ.get("CAPTION", "")
 
-
-
+Current_Thumb = environ.get("THUMBNAIL", "")
+THUMBNAIL = [set(x) for x in (Current_Thumb).split()]                 
 
 
 
