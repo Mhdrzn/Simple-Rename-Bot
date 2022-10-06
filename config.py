9130739229ac235d2ff4af1ec0b6341052bb026b
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-10-06 21:36:29 +0530

    class

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))
-CAPTION = environ.get("CAPTION", "")
+class config(object):
+   THUMBNAIL = None
+   API_ID = int(environ.get("API_ID", ""))
+   API_HASH = environ.get("API_HASH", "")
+   BOT_TOKEN = environ.get("BOT_TOKEN", "")
+   ADMIN = int(environ.get("ADMIN", ""))
+   CAPTION = environ.get("CAPTION", "")
 
 
 
