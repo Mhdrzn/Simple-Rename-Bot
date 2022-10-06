282e9012c1f57b12c08175e32926b23f6e068ccc
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-06 21:39:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))
 CAPTION = environ.get("CAPTION", "")
+THUMBNAIL = os.environ.get("THUMBNAIL", "")
 
 
 
