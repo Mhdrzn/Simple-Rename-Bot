5bbc3de6796213e8bd1f0860f0fb85d8027bb537
Mhdrzn <103956396+Mhdrzn@users.noreply.github.com>
2022-10-06 21:39:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 from pyrogram import Client
-from config import *
+from config import config
 from pyromod import listen
 
 class App(Client):
     def __init__(self):
         super().__init__(
             name="renamer",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
             workers=50,
             plugins={"root": "main"},
             sleep_threshold=5,
